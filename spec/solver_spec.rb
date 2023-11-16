9a16f456fdf3d0ea074e9554965c3f9d655173fc
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-16 20:48:18 +0200

    Describe reserve word on test file and test return error undefined method everse' for Solver:Class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,10 @@
 require './solver'
 
 RSpec.describe Solver do
+
+  describe 'reverse word' do
+    it 'reverses a word' do
+      expect(Solver.reverse("hello")).to eq("olleh")
+    end
+  end
 end
