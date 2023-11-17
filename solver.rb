f660dc020119e2aa792244e6dcad3d5518da66e6
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-17 11:31:30 +0200

    Add conditional statement on fizzbuzz method, that checks if num is equally divisible by 3, 5, 3 && 5 and returns either fizz, fizz, fizzbuzz and any case number is converted to string

--- solver.rb
+++ solver.rb
@@ -4,5 +4,15 @@ class Solver
   end
 
   def self.fizzbuzz(num)
+    if(num % 3 == 0) && (num % 5 == 0)
+      'fizzbuzz'
+    elsif (num % 3 == 0)
+      'fizz'
+    elsif (num % 5 == 0)
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
+puts Solver.fizzbuzz(7)
