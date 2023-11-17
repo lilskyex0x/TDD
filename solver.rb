f378525dd6c1d8d7b9f9d96c41916959c3810824
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-17 11:44:06 +0200

    Fix robocop offenses, it recommended I use .zero? method as opposed to == 0 on my conditional statements

--- solver.rb
+++ solver.rb
@@ -4,15 +4,14 @@ class Solver
   end
 
   def self.fizzbuzz(num)
-    if(num % 3 == 0) && (num % 5 == 0)
+    if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
-    elsif (num % 3 == 0)
+    elsif (num % 3).zero?
       'fizz'
-    elsif (num % 5 == 0)
+    elsif (num % 5).zero?
       'buzz'
     else
       num.to_s
     end
   end
 end
-puts Solver.fizzbuzz(7)
