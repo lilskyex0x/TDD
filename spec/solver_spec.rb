55642ea91512d5d8f52ec6571a8697ee46156157
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-17 11:12:56 +0200

    Described fizzbuzz method on test file, run test and all 4 test cases failed with error: undefined method izzbuzz' for Solver:Class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Solver do
       expect(Solver.reverse('hello')).to eq('olleh')
     end
   end
+
+  describe 'Fizzbuzz?' do
+    it 'returns "fizz" when N is divisible by 3' do
+      expect(Solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" when N is divisible by 5' do
+      expect(Solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" when N is divisible by 3 and 5' do
+      expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns number as a string for any other case' do
+      expect(Solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
