a0e7ec6c72e59bde28da6ad9281d7659f74dd5ba
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-11-17 18:38:26 +0630

    Add logic that raise argument error if number is negative else returns factorial

--- solver.rb
+++ solver.rb
@@ -1,7 +1,10 @@
 class Solver
   def self.factorial(number)
+    raise ArgumentError, 'Factorial is not defined for negative numbers' if number.negative?
+
+    (1..number).inject(1, :*)
   end
-  
+
   def self.reverse(word)
     word.reverse
   end
